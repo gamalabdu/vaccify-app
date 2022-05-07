57e3721bac19f1302692ddf993ff5135a960d516
Gamal Abdu <abdu.s.gamal@gmail.com>
2022-05-07 15:43:20 -0400

    trying to fix iphone compatibility

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
     const navigate = useNavigate()
     const [menuOpen, setMenuOpen] = useState(false)
     const [size, setSize] = useState({
-        width: 0,
-        height: 0,
+        width: 300,
+        height: 300,
     });
 
     useEffect(() => {
